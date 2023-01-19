485b967eaea980c0bfb31efc5549bacdcd27e7fe
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-01-19 15:14:36 +0900

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,173 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'NinoHub',
+  tagline: 'アプリ開発する人',
+  url: 'https://io.github.com.ninohub',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'dninomiya', // Usually your GitHub org/user name.
+  projectName: 'ninohub', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ja',
+    locales: ['ja'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+        title: 'NinoHub',
+        logo: {
+          alt: 'NinoHub Logo',
+          src: 'img/logo.PNG',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'web/intro',
+            position: 'left',
+            label: '基礎知識',
+            className: 'header-vscode-link',
+          },
+          {
+            type: 'doc',
+            docId: 'web/intro',
+            position: 'left',
+            className: 'header-next-link',
+            label: 'Webアプリ',
+          },
+          {
+            type: 'doc',
+            docId: 'flutter/intro',
+            position: 'left',
+            className: 'header-flutter-link',
+            label: 'モバイルアプリ',
+          },
+          {
+            type: 'doc',
+            docId: 'firebase/intro',
+            position: 'left',
+            className: 'header-firebase-link',
+            label: 'Firebase',
+          },
+          {
+            href: 'https://www.youtube.com/channel/UCUPq5dKFGnOziaqYI-ejYcg',
+            className: 'header-youtube-link',
+            'aria-label': 'YouTube',
+            position: 'right',
+          },
+          {
+            href: 'https://discord.gg/QeKNfAw6cK',
+            className: 'header-discord-link',
+            'aria-label': 'Discord',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'NinoGuideについて',
+                to: '/',
+              },
+              {
+                label: 'Webアプリ',
+                to: '/web/intro',
+              },
+              {
+                label: 'NinoHubについて',
+                to: '/flutter/intro',
+              },
+              {
+                label: 'Firebase',
+                to: '/firebase/intro',
+              },
+            ],
+          },
+          {
+            title: 'リンク',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/QeKNfAw6cK',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/d151005',
+              },
+              {
+                label: 'YouTube',
+                href: 'https://www.youtube.com/channel/UCUPq5dKFGnOziaqYI-ejYcg',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Deer, Inc.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
